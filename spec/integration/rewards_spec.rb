703f2cbb1e1566f627124a39c23f5b937dfe918c
Sebastian Nanek <snanek@gmail.com>
2015-03-01 23:11:41 +0000

    Integration test for the application

--- spec/integration/rewards_spec.rb
+++ spec/integration/rewards_spec.rb
@@ -8,4 +8,36 @@ RSpec.describe 'RewardsService' do
       expect(last_response).to be_ok
     end
   end
+
+  describe '/rewards' do
+    let(:params) do
+      {
+        account_number: "CATCH22",
+        channel_subscriptions: [ "KIDS", "SPORTS" ]
+      }
+    end
+
+    it 'returns 200' do
+      get '/rewards', params
+
+      expect(last_response).to be_ok
+    end
+
+    it 'returns false as error in response body' do
+      get '/rewards', params
+
+      parsed_response = JSON.parse(last_response.body)
+
+      expect(parsed_response["error"]).to eq(false)
+    end
+
+    it 'returns example reward' do
+      get '/rewards', params
+
+      parsed_response = JSON.parse(last_response.body)
+
+      expect(parsed_response["rewards"]).
+        to include("CHAMPIONS_LEAGUE_FINAL_TICKET")
+    end
+  end
 end
