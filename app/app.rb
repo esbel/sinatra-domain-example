ce5f8cd94b72b91f7c5224613550f540ee34dbb0
Sebastian Nanek <snanek@gmail.com>
2015-03-01 22:54:17 +0000

    Require JSON and RewardsRule

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,10 @@
+require 'json'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
 require_relative 'domain/channel'
 require_relative 'domain/reward'
+require_relative 'business_rules/rewards_rule'
 require_relative 'service_objects/eligibility_service'
 require_relative 'service_objects/rewards_service'
 
