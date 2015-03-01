51497559e3cfd067594d97819ece05d263da426c
Sebastian Nanek <snanek@gmail.com>
2015-03-01 13:48:36 +0000

    RSpec config cleanup

--- spec/integration/rewards_spec.rb
+++ spec/integration/rewards_spec.rb
@@ -1,14 +1,8 @@
 require_relative '../spec_helper.rb'
 
 RSpec.describe 'RewardsService' do
-  include Rack::Test::Methods
-
-  def app
-    App
-  end
-
   describe '/' do
-    it "returns 200" do
+    it 'returns 200' do
       get '/'
 
       expect(last_response).to be_ok
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,8 @@ require 'rack/test'
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
+
+  def app
+    App
+  end
 end
